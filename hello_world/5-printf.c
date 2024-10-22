b66dd65109ed792cbb2a09eada2d4e6d6233b4ff
korneevscp <vladimirivanof2024@gmail.com>
2024-10-22 10:56:05 +0200

    add new

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,14 @@
 /*
  * File: 5-printf.c
- * my code by korneevscp 
  */
 
 #include <stdio.h>
 
 /**
- * Return: Always 0.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," .
+ *
+ * Return: stope 0.
  */
 int main(void)
 {
