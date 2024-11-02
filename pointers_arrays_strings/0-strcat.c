a1826b610d63e5625a2d2de4791de5637fa5565c
korneevscp <vladimirivanof2024@gmail.com>
2024-11-02 23:23:00 +0100

    new 0.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
