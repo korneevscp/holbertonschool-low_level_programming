3dcb70ccb8d7fe0b4f2ae20d4f87f3addaac3e40
korneevscp <vladimirivanof2024@gmail.com>
2024-12-05 02:56:19 +0100

    add

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,56 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: a pointer to a string of '0' and '1' chars
+ * 
+ * Return: the converted number, or 0 if there is one or more chars
+ *         in the string b that is not '0' or '1' or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+    unsigned int result = 0;
 
-	if (*b == '\0')
-		return (0);
+    // Check if the string is NULL
+    if (b == NULL)
+        return (0);
 
-	for (len = 0; b[len];)
-		len++;
+    // Traverse the string
+    while (*b)
+    {
+        // If the character is not '0' or '1', return 0
+        if (*b != '0' && *b != '1')
+            return (0);
 
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+        // Shift the result to the left by 1 to make space for the next bit
+        result = result << 1;
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
+        // If the current character is '1', set the least significant bit
+        if (*b == '1')
+            result |= 1;
 
-	return (num);
+        // Move to the next character
+        b++;
+    }
+
+    return result;
+}
+
+int main(void)
+{
+    unsigned int n;
+
+    // Testing with different binary strings
+    n = binary_to_uint("1");
+    printf("%u\n", n);  // Should print 1
+    n = binary_to_uint("101");
+    printf("%u\n", n);  // Should print 5
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);  // Should print 0 (invalid character 'e')
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);  // Should print 98
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);  // Should print 402
+
+    return (0);
 }
